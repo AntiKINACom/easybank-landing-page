64162672448736072fae563353f499c803baad32
boileryju <boileryju@gmail.com>
2021-01-16 07:53:53 +0800

    auto reformat

--- app/js/script.js
+++ app/js/script.js
@@ -5,21 +5,21 @@ const overlay = document.querySelector(".overlay");
 const fadeElements = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", function () {
-  if (header.classList.contains("open")) {
-    // Close Hamburger Menu
-    body.classList.remove("noscroll");
-    header.classList.remove("open");
-    fadeElements.forEach(function (element) {
-      element.classList.remove("fade-in");
-      element.classList.add("fade-out");
-    });
-  } else {
-    // Open Hamburger Menu
-    body.classList.add("noscroll");
-    header.classList.add("open");
-    fadeElements.forEach(function (element) {
-      element.classList.remove("fade-out");
-      element.classList.add("fade-in");
-    });
-  }
+    if (header.classList.contains("open")) {
+        // Close Hamburger Menu
+        body.classList.remove("noscroll");
+        header.classList.remove("open");
+        fadeElements.forEach(function (element) {
+            element.classList.remove("fade-in");
+            element.classList.add("fade-out");
+        });
+    } else {
+        // Open Hamburger Menu
+        body.classList.add("noscroll");
+        header.classList.add("open");
+        fadeElements.forEach(function (element) {
+            element.classList.remove("fade-out");
+            element.classList.add("fade-in");
+        });
+    }
 });
